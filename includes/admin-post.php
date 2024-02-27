ba22bba6b87c8b1aae43a31bf557d5e932e6ebce
Juzaweb <dangtheanh16@gmail.com>
2024-02-27 23:33:44 +0700

    :+1: Translate posts

--- includes/admin-post.php
+++ includes/admin-post.php
@@ -1,30 +1,48 @@
 <?php
 
-add_filter('bulk_actions-edit-post', 'wtc_register_translate_actions');
+// Add the translation action
+add_filter('bulk_actions-edit-post', 'wtc_register_translate_action');
 
-function wtc_register_translate_actions($bulk_actions)
+function wtc_register_translate_action($bulk_actions)
 {
     $bulk_actions['translate'] = __('Translate to', 'wtc');
-
-    //add_thickbox();
 
     return $bulk_actions;
 }
 
-//add_filter('handle_bulk_actions-edit-post', 'wtc_translate_action_handler', 10, 3);
+// Add the custom columns to the post types
+add_filter( 'manage_posts_columns', 'set_custom_edit_book_columns' );
+function set_custom_edit_book_columns($columns) {
+    unset( $columns['author'] );
+    $columns['translate_versions'] = __( 'Translate Versions', 'wtc' );
 
-function wtc_translate_action_handler($redirect_to, $doaction, $post_ids)
-{
-    if ($doaction !== 'translate') {
-        return $redirect_to;
-    }
-
-    $languages = $_GET['languages'];
-
-    foreach ($post_ids as $post_id) {
-        // Perform action for each post.
-    }
-
-    return add_query_arg('bulk_emailed_posts', count($post_ids), $redirect_to);
+    return $columns;
 }
 
+add_action( 'manage_posts_custom_column' , 'custom_book_column', 10, 2 );
+function custom_book_column( $column, $post_id ) {
+    if ($column == 'translate_versions') {
+        $terms = get_the_term_list( $post_id , 'book_author' , '' , ',' , '' );
+        if ( is_string( $terms ) )
+            echo $terms;
+        else
+            _e( 'Unable to get author(s)', 'your_text_domain' );
+    }
+}
+
+// add_filter('handle_bulk_actions-edit-post', 'wtc_translate_action_handler', 10, 3);
+
+// function wtc_translate_action_handler($redirect_to, $doaction, $post_ids)
+// {
+//     if ($doaction !== 'translate') {
+//         return $redirect_to;
+//     }
+//
+//     $languages = $_GET['languages'];
+//
+//     foreach ($post_ids as $post_id) {
+//         // Perform action for each post.
+//     }
+//
+//     return add_query_arg('bulk_emailed_posts', count($post_ids), $redirect_to);
+// }
--- includes/ajax.php
+++ includes/ajax.php
@@ -13,7 +13,7 @@ function wtc_ajax_post_translate_handler()
     $post = get_post($post_id);
 
     $query = $wpdb->prepare("SELECT * FROM {$wpdb->prefix}wtc_post_content WHERE post_id = %s;", $post->ID);
-    $row = $wpdb->get_row( $query );
+    $row = $wpdb->get_row($query);
 
     if (function_exists('wpm_translate_object') && wtc_is_wp_multilang_support()) {
         $post = wpm_translate_object($post, $default_locale);
@@ -32,6 +32,38 @@ function wtc_ajax_post_translate_handler()
             'remote_content_id' => $remote_post['id'],
             'created_at' => date('Y-m-d H:i:s'),
         ]);
+    }
+
+    $translate_log = $wpdb->get_row(
+        $wpdb->prepare(
+            "SELECT * FROM {$wpdb->prefix}wtc_translate_histories WHERE post_id = %s AND locale = %s;",
+            $post->ID,
+            $to_locale
+        )
+    );
+
+    if ($translate_log && $translate_log->status != 'error') {
+        wp_send_json([]);
+    }
+
+    if (empty($translate_log)) {
+        $translate_log = $wpdb->insert("{$wpdb->prefix}wtc_translate_histories", [
+            'post_id' => $post->ID,
+            'new_post_id' => $post->ID,
+            'remote_content_id' => $row->remote_content_id,
+            'locale' => $to_locale,
+            'created_at' => date('Y-m-d H:i:s'),
+            'status' => 'pending',
+        ]);
+    } else {
+        $wpdb->update("{$wpdb->prefix}wtc_translate_histories",
+            [
+                'status' => 'pending',
+            ],
+            [
+                'id' => $translate_log->id,
+            ]
+        );
     }
 
     wp_send_json($api->translate($row->remote_content_id, $to_locale));
