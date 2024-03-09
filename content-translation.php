4a5e9456ea68f917a164f903055f1348e7eff3dc
Juzaweb <dangtheanh16@gmail.com>
2024-03-09 11:01:04 +0700

    :+1: Add foreign key to posts table

--- content-translation.php
+++ content-translation.php
@@ -4,7 +4,7 @@
  * Description: Automatic translate content for your WordPress site.
  * Version: 1.0
  * Author: My Crawlers
- * Author URI: https://mycrawlers.com
+ * Author URI: https://juzaweb.com
  * Text Domain: wtc
  * Requires at least: 6.3
  * Requires PHP: 7.4
@@ -25,23 +25,26 @@ function wtc_create_database_table()
     $charset_collate = $wpdb->get_charset_collate();
 
     $sqls[] = "CREATE TABLE {$wpdb->prefix}wtc_post_content (
-        id mediumint(9) NOT NULL AUTO_INCREMENT,
-        post_id mediumint(9) NOT NULL,
-        remote_content_id mediumint(9) NOT NULL,
+        id bigint(9) NOT NULL AUTO_INCREMENT,
+        post_id bigint(20) UNSIGNED NOT NULL,
+        remote_content_id bigint(20) NOT NULL,
         created_at datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
-        PRIMARY KEY  (id)
+        PRIMARY KEY  (id),
+        FOREIGN KEY (post_id) REFERENCES {$wpdb->prefix}posts(ID) ON DELETE CASCADE
     ) $charset_collate;";
 
     $sqls[] = "CREATE TABLE {$wpdb->prefix}wtc_translate_histories (
-        id mediumint(9) NOT NULL AUTO_INCREMENT,
-        post_id mediumint(9) NOT NULL,
-        new_post_id mediumint(9) NULL,
-        remote_content_id mediumint(9) NULL,
+        id bigint(9) NOT NULL AUTO_INCREMENT,
+        post_id bigint(20) UNSIGNED NOT NULL,
+        new_post_id bigint(20) UNSIGNED NULL,
+        remote_content_id bigint(20) NULL,
         locale varchar(10) NOT NULL,
         created_at datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
         status varchar(10) NOT NULL DEFAULT 'pending',
         error text NULL,
-        PRIMARY KEY  (id)
+        PRIMARY KEY  (id),
+        FOREIGN KEY (post_id) REFERENCES {$wpdb->prefix}posts(ID) ON DELETE CASCADE,
+        FOREIGN KEY (new_post_id) REFERENCES {$wpdb->prefix}posts(ID) ON DELETE CASCADE
     ) $charset_collate;";
 
     require_once ABSPATH.'wp-admin/includes/upgrade.php';
